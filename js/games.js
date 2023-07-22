da5871d5333a059e7195734a63ccb485f768c056
Sky <skysth3limit@pm.me>
2023-07-22 18:42:28 -0400

    game loading works :sunglasses:

--- js/games.js
+++ js/games.js
@@ -1,5 +1,16 @@
 $.getJSON("/games.json", function (data) {
     for (let i = 0; i < data.length; i++) {
-        console.log(data[i].name);
+        $('#games').append(
+            $('<div>').prop({
+                id: 'game',
+                style: 'cursor: pointer;'
+            }).append(
+                $('<img>').prop({
+                    src: data[i].directory + "/" + data[i].image
+                })
+            ).append(
+                $('<h1>').text(data[i].name)
+            )
+        );
     } 
 })
