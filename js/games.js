bc112101d2a7b9ce3841c244b84ed155cd1cff78
Sky <skysth3limit@pm.me>
2023-07-27 21:26:28 -0400

    pinned games

--- js/games.js
+++ js/games.js
@@ -1,33 +1,91 @@
 $.getJSON("/games.json", function (data) {
+    
+    starredgames = getCookie("starred");
+    if(starredgames == "") {
+        starredgames = []
+    } else {
+        starredgames = JSON.parse(starredgames)
+
+    }
+    console.log(starredgames);
     $("#gamesearch").prop({placeholder: "Click here to search through our " + data.length + " games!"});
     data.sort(dynamicSort("name"));
     for (let i = 0; i < data.length; i++) {
-        $('#games').append(
-            $('<div>').prop({
-                id: 'game',
-                style: 'cursor: pointer;',
-                dir: data[i].directory
-            }).append(
-                $('<img>').prop({
-                    src: data[i].directory + "/" + data[i].image,
-                    alt: data[i].name + " logo"
-                })
-            ).append(
-                $('<h1>').text(data[i].name)
-            )
+        console.log("Loop iteration:", i);
+        let $element = $('<div>').prop({
+            class: 'game',
+            style: 'cursor: pointer;',
+            id: data[i].directory
+        }).append(
+            $('<img>').prop({
+                src: data[i].directory + "/" + data[i].image,
+                alt: data[i].name + " logo"
+            })
+        ).append(
+            $('<h1>').text(data[i].name)
+        ).append(
+            $("<mat-icon>").prop({
+                class: 'material-symbols-rounded'
+            }).text("star")
         );
+    
+        if (starredgames.includes(data[i].directory)) {
+            $element.find("mat-icon").attr("id", "starred");
+            let $pinnedelement = $element.clone();
+            $('#pinnedgames').append($pinnedelement);
+            if($("#pinnedgames #message")) {
+                $("#pinnedgames #message").hide();
+            }
+           
+        }
+        
+        $('#games').append($element);
+
     }
     $("#games #message").remove();
 });
 
 $(document).ready(function() {
-    $(document).on("click", "#game", function(event) {
-        redirectGame($(this).attr("dir"));
+    let starred;
+    $(document).on("click", ".game", function(event) {
+        if($(event.target).is('mat-icon')) {
+            if(!$(event.target).attr("id")) {
+                $(event.target).prop({id: "starred"})
+                starred = Cookies.get("starred")
+                if(starred) {
+                    starred = JSON.parse(starred);
+                } else {
+                    starred = []
+                }
+                starred.push($(this).attr("id"));
+                Cookies.set("starred", JSON.stringify(starred));
+                $element = $(this).clone();
+                $('#pinnedgames').append($element);
+                $("#pinnedgames #message").hide();
+            } else {
+                $(event.target).removeAttr("id");
+                $thisdiv = '#' + $(this).attr("id")
+                starred = Cookies.get("starred")
+                starred = JSON.parse(starred);
+                starred.pop(starred.indexOf($(this).attr("dir")), starred.indexOf($(this).attr("dir")));
+                Cookies.set("starred", JSON.stringify(starred));
+                $("#pinnedgames " + $thisdiv).remove();
+                if(!$("#pinnedgames div") == "") {
+                    $("#pinnedgames #message").show();
+                }
+                $($thisdiv + " #starred").removeAttr("id");
+            }
+        } else {
+            redirectGame($(this).attr("id"));
+        }
+    }); 
+    $(document).on("click", "#game span", function(event) {
+        $(this).prop({class: "material-symbols-outlined fill"})
     });    
 });
 
 function redirectGame(dir) {
-     window.location.href = window.location.origin + "/" + dir + "/index.html";
+    window.location.href = window.location.origin + "/" + dir + "/index.html";
 }
 function dynamicSort(property) {
     var sortOrder = 1;
--- js/search.js
+++ js/search.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $('#gamesearch').on('input propertychange paste', function() {
-        $('#games #game').hide();
+        $('#games .game').hide();
         var txt = $('#gamesearch').val();
-        $('#games #game').each(function(){
+        $('#games .game').each(function(){
            if($(this).text().toUpperCase().indexOf(txt.toUpperCase()) != -1){
                $(this).show();
            }
