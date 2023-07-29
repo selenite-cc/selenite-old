c0af10de00c4dc1c7f3a4a2c564e30a98c7d2125
Sky <skysth3limit@pm.me>
2023-07-28 20:28:34 -0400

    use prettier to make code.. prettier

--- js/cloak.js
+++ js/cloak.js
@@ -1,34 +1,35 @@
 function cloakExceptions(url) {
-    if (url.includes('harrisonburg.instructure.com') == true) {
-        return "learn.canvas.net";
-    }
-    return url;
+  if (url.includes("harrisonburg.instructure.com") == true) {
+    return "learn.canvas.net";
+  }
+  return url;
 }
 
 function setCloakCookie() {
-    url = cloakExceptions($('#webicon').val())
-    document.cookie = 'tabicon=https://s2.googleusercontent.com/s2/favicons?domain_url=' + url;
-    document.cookie = 'tabname=' + $('#webname').val();
-    Toastify({
-        text: 'Tab cloak saved!',
-        duration: 2000,
-        gravity: "top", // `top` or `bottom`
-        position: "center", // `left`, `center` or `right`
-        style: {
-          background: "linear-gradient(42deg, rgba(36, 69, 128, 1) 100%, rgb(24, 17, 87) 0%)"
-        },
-        onClick: function(){} // Callback after click
-      }).showToast();
-    setCloak();
+  url = cloakExceptions($("#webicon").val());
+  document.cookie = "tabicon=https://s2.googleusercontent.com/s2/favicons?domain_url=" + url;
+  document.cookie = "tabname=" + $("#webname").val();
+  Toastify({
+    text: "Tab cloak saved!",
+    duration: 2000,
+    gravity: "top", // `top` or `bottom`
+    position: "center", // `left`, `center` or `right`
+    style: {
+      background: "linear-gradient(42deg, rgba(36, 69, 128, 1) 100%, rgb(24, 17, 87) 0%)",
+    },
+    onClick: function () {}, // Callback after click
+  }).showToast();
+  setCloak();
 }
 
 function clearCloak() {
-    document.cookie = "tabicon=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    document.cookie = "tabname=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    var link = document.querySelector("link[rel~='icon']");link.remove();
-    document.title = "Tab Cloak | e-gamepass";
-    link = document.createElement('link');
-    link.rel = 'icon';
-    document.head.appendChild(link);
-    link.href = "/favicon.png";
+  document.cookie = "tabicon=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  document.cookie = "tabname=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  var link = document.querySelector("link[rel~='icon']");
+  link.remove();
+  document.title = "Tab Cloak | e-gamepass";
+  link = document.createElement("link");
+  link.rel = "icon";
+  document.head.appendChild(link);
+  link.href = "/favicon.png";
 }
--- js/cookie.js
+++ js/cookie.js
@@ -2,7 +2,7 @@
 function getMainSave() {
   var mainSave = {};
   // List of items in localStorage that should not be saved
-  var localStorageDontSave = ['tabicon', 'tabname'];
+  var localStorageDontSave = ["tabicon", "tabname"];
 
   // Convert localStorage to an array of key-value pairs and remove the items that should not be saved
   localStorageSave = Object.entries(localStorage);
@@ -28,7 +28,7 @@ function getMainSave() {
   mainSave = btoa(JSON.stringify(mainSave));
 
   // Encrypt the mainSave data using AES encryption with the key 'save'
-  mainSave = CryptoJS.AES.encrypt(mainSave, 'egamepass').toString();
+  mainSave = CryptoJS.AES.encrypt(mainSave, "egamepass").toString();
 
   // Return the encrypted mainSave data
   return mainSave;
@@ -39,9 +39,9 @@ function downloadMainSave() {
   var data = new Blob([getMainSave()]);
   var dataURL = URL.createObjectURL(data);
 
-  var fakeElement = document.createElement('a');
+  var fakeElement = document.createElement("a");
   fakeElement.href = dataURL;
-  fakeElement.download = 'your.e-gamepass.save';
+  fakeElement.download = "your.e-gamepass.save";
   fakeElement.click();
   URL.revokeObjectURL(dataURL);
   Toastify({
@@ -52,14 +52,14 @@ function downloadMainSave() {
     style: {
       background: "linear-gradient(42deg, rgba(36, 69, 128, 1) 100%, rgb(24, 17, 87) 0%)",
     },
-    onClick: function(){} // Callback after click
+    onClick: function () {}, // Callback after click
   }).showToast();
 }
 
 // Function to get the main save data from an uploaded file
 function getMainSaveFromUpload(data) {
   // Decrypt the uploaded data using AES decryption with the key 'save'
-  data = CryptoJS.AES.decrypt(data, 'egamepass').toString(CryptoJS.enc.Utf8);
+  data = CryptoJS.AES.decrypt(data, "egamepass").toString(CryptoJS.enc.Utf8);
 
   // Parse the decrypted data as JSON
   var mainSave = JSON.parse(atob(data));
@@ -77,11 +77,11 @@ function getMainSaveFromUpload(data) {
 
 // Function to handle the file upload
 function uploadMainSave() {
-  var hiddenUpload = document.querySelector('.hiddenUpload');
+  var hiddenUpload = document.querySelector(".hiddenUpload");
   hiddenUpload.click();
 
   // Listen for the change event on the file input element
-  hiddenUpload.addEventListener('change', function (e) {
+  hiddenUpload.addEventListener("change", function (e) {
     var files = e.target.files;
     var file = files[0];
     if (!file) {
@@ -104,9 +104,8 @@ function uploadMainSave() {
         style: {
           background: "linear-gradient(42deg, rgba(36, 69, 128, 1) 100%, rgb(24, 17, 87) 0%)",
         },
-        onClick: function(){} // Callback after click
+        onClick: function () {}, // Callback after click
       }).showToast();
-      
     };
 
     reader.readAsText(file);
