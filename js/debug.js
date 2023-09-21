bc0b52d478268dbc1899d765f70e7e00f56108f3
Sky <skysth3limit@pm.me>
2023-09-20 20:08:04 -0400

    gam and fix debug

--- js/debug.js
+++ js/debug.js
@@ -2,12 +2,7 @@ window.onerror = function(msg, url, linenumber) {
     alert('Error message: '+msg+'\nURL: '+url+'\nLine Number: '+linenumber);
     return true;
 }
-let listofchars = "";
-document.onkeydown = function (e) {
-    listofchars = listofchars + e.key;
-    if(listofchars.length > 50) {
-        listofchars = listofchars.substring(e.key.length);
-    }
+document.onkeyup = function (e) {
     if(listofchars.includes("runsomejs")) {
         alert(eval(prompt("js?")));
         listofchars = "";
