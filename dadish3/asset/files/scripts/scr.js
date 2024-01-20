d1841034dd46d0e948ec209bbb94afe497aa4471
khaybarkhaybaryayahud <156494330+khaybarkhaybaryayahud@users.noreply.github.com>
2024-01-19 19:39:13 -0500

    Add files via upload

---
+++ dadish3/asset/files/scripts/scr.js
@@ -0,0 +1,11 @@
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym");
+
+   ym(93577277, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });
